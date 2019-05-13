ca999deee7c63f81246dbd59588f20b5c10b9668
Heather Hartley <hlhartley@gmail.com>
2019-05-12 22:15:09 -0600

    Examples of instance vs class methods

--- app/controllers/rings_controller.rb
+++ app/controllers/rings_controller.rb
@@ -1,9 +1,11 @@
 class RingsController < ApplicationController
     def index
         @rings = Ring.all()
+        @average_price = Ring.average_price
     end
 
     def show
+        # Here we found an INSTANCE (one) ring:
         @ring = Ring.find(params["id"])
     end
 end
--- app/models/ring.rb
+++ app/models/ring.rb
@@ -1,2 +1,19 @@
 class Ring < ApplicationRecord
+
+    # INSTANCE METHOD (here we are just looking at 1 ring)
+    # Meaning we create or have an instance of a ring and want its price with tax:
+    def price_with_tax
+        # Here self refers to the instance of the ring 
+        self.price + (0.08 * self.price)
+    end
+
+    # CLASS METHOD (looking at ALL the rings)
+    # Here we want average price of all the rings, so there is no one instance of a ring
+    # that we are looking at:
+
+    # Here self refers to the Ring class
+    def self.average_price
+        Ring.average(:price)
+    end
+
 end
